1ba8b28abe73c92839f52dfb7ba30d17295c2607
Arnaud UTILLE <arnaudutille@Mac-541.lan>
2026-05-06 10:54:11 -0400

    Add ABM BTP grands comptes blog article

--- src/utils/blogImages.ts
+++ src/utils/blogImages.ts
@@ -38,6 +38,7 @@ import googleAdsKeywordPlannerRenovation from "@/assets/blog/google-ads-keyword-
 import communicationBtpLevierStrategiquePositionnement from "@/assets/blog/communication-btp-levier-strategique-positionnement.png";
 import strategieMarketingCouverture2026 from "@/assets/blog/strategie-marketing-couverture-2026.png";
 import videoDroneBtpVisibiliteReassurance from "@/assets/blog/video-drone-btp-visibilite-reassurance.png";
+import abmBtpCiblerGrandsComptesOpportunites2026 from "@/assets/blog/abm-btp-cibler-grands-comptes-opportunites-2026.png";
 
 const coverImages: Record<string, string> = {
   // Anciennes clés (compat)
@@ -98,6 +99,7 @@ const coverImages: Record<string, string> = {
   "communication-btp-levier-strategique-positionnement": communicationBtpLevierStrategiquePositionnement,
   "strategie-marketing-couverture-2026": strategieMarketingCouverture2026,
   "video-drone-btp-visibilite-reassurance": videoDroneBtpVisibiliteReassurance,
+  "abm-btp-cibler-grands-comptes-opportunites-2026": abmBtpCiblerGrandsComptesOpportunites2026,
 };
 
 export const getCoverImage = (key: string): string => {
